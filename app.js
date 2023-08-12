a365ead43545ae25b1e8d17a70fa8424f5c53ddf
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-08-12 18:02:08 +0330

    add note project
    ezfrh kardn note b project

--- app.js
+++ app.js
@@ -1,4 +1,30 @@
 // 1402-05-18
 // noteList-project
 // gr:Amin
-// discription: here we can wwrite our daily notes.
+// discription: here we can write our daily notes.
+
+let main = document.querySelector('#main');
+let noteList = document.querySelector('#note-list');
+
+main.addEventListener('click',newNote);
+
+function newNote (e){
+    e.preventDefault();
+    let userText = document.querySelector('#no').value
+
+    let span = document.createElement('span')
+    span.innerText = userText
+
+    let li = document.createElement('li')
+    li.appendChild(span)
+
+    let a = document.createElement('a')
+    a.innerText = 'X'
+    a.setAttribute('href','#')
+    a.classList.add('remove-note')
+    
+    // add removeBtn to li
+    li.appendChild(a)
+    // add li to note List
+    noteList.appendChild(li)
+}
